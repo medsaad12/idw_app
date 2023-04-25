12d462227dad705cdc4ae564bf327c4c528e93e9
Sifeddine reguragui <sifeddine.regragui@gmail.com>
2023-04-25 16:38:57 +0000

    sifebar appearence improved

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -17,17 +17,17 @@
     <div class="sidebar">
       <div class="sidelinks">
         <a href="{{ url('profile') }}" class="link">
-          <img src="{{ asset('svgs/person-fill.svg') }}" class="icons">
+          <img src="{{ asset('svgs/person.svg') }}" class="icons">
           <span>Profile</span>
         </a>
         @if (Auth::user()->hasRole(['ADMIN', 'RH' ,'CE' ,'AGENT']))
         @role('ADMIN')
         <a href="{{ url('chat') }}" class="link">
-          <img src="{{ asset('svgs/chat-left-dots-fill.svg') }}" class="icons">
+          <img src="{{ asset('svgs/chat-left-dots.svg') }}" class="icons">
           <span>Chat</span>
         </a>
         <a href="{{ url('groupes') }}" class="link">
-          <img src="{{ asset('svgs/people-fill.svg') }}" class="icons">
+          <img src="{{ asset('svgs/people.svg') }}" class="icons">
           <span>Groupes</span>
         </a>
         <a href="{{ url('users') }}" class="link">
@@ -50,11 +50,11 @@
         @endrole
         @role('AGENT')
         <a href="{{ url('chat') }}" class="link">
-          <img src="{{ asset('svgs/chat-left-dots-fill.svg') }}" class="icons">
+          <img src="{{ asset('svgs/chat-left-dots.svg') }}" class="icons">
           <span>Chat</span>
         </a>
         <a href="{{ url('groupes') }}" class="link">
-          <img src="{{ asset('svgs/people-fill.svg') }}" class="icons">
+          <img src="{{ asset('svgs/people.svg') }}" class="icons">
           <span>Groupes</span>
         </a>
         <a href="/forms" class="link">
@@ -64,15 +64,15 @@
         @endrole
         @role('CE')
         <a href="{{ url('chat') }}" class="link">
-          <img src="{{ asset('svgs/chat-left-dots-fill.svg') }}" class="icons">
+          <img src="{{ asset('svgs/chat-left-dots.svg') }}" class="icons">
           <span>Chat</span>
         </a>
         <a href="{{ url('groupes') }}" class="link">
-          <img src="{{ asset('svgs/people-fill.svg') }}" class="icons">
+          <img src="{{ asset('svgs/people.svg') }}" class="icons">
           <span>Groupes</span>
         </a>
         <a href="{{ url('codes') }}" class="link">
-          <img src="{{ asset('svgs/people-fill.svg') }}" class="icons">
+          <img src="{{ asset('svgs/people.svg') }}" class="icons">
           <span>Codes</span>
         </a>
         <a href="{{ url('tableauDesAgents') }}" class="link">
@@ -86,15 +86,15 @@
         @endrole
         @role('RH')
         <a href="{{ url('chat') }}" class="link">
-          <img src="{{ asset('svgs/chat-left-dots-fill.svg') }}" class="icons">
+          <img src="{{ asset('svgs/chat-left-dots.svg') }}" class="icons">
           <span>Chat</span>
         </a>
         <a href="{{ url('formations') }}" class="link">
-          <img src="{{ asset('svgs/people-fill.svg') }}" class="icons">
+          <img src="{{ asset('svgs/people.svg') }}" class="icons">
           <span>Groupes</span>
         </a>
         <a href="/formations" class="link">
-          <img width="36" height="38" src="{{ asset('svgs/formation.png') }}" class="icons">
+          <img src="{{ asset('svgs/mortarboard.svg') }}" class="icons">
           <span>Gestion de formations</span>
         </a>
         <a href="{{ url('StatistiqueDesAgent') }}" class="link">
@@ -102,37 +102,37 @@
           <span>Agents</span>
         </a>
         <a href="/presence" class="link">
-          <img width="3" height="35" src="{{ asset('svgs/presence.png') }}" class="icons">
+          <img src="{{ asset('svgs/person-check.svg') }}" class="icons">
           <span>Tableau de présence</span>
         </a>
         <a href="/entretiens" class="link">
-          <img height="30" width="35" src="{{ asset('svgs/entretien.png') }}" class="icons">
+          <img src="{{ asset('svgs/person-workspace.svg') }}" class="icons">
           <span>Gestion des entretiens</span>
         </a>
         <a href="{{ url('calculatrice') }}" class="link">
-          <img width="38" height="35" src="{{ asset('svgs/calculatrice.png') }}" class="icons">
+          <img src="{{ asset('svgs/calculator.svg') }}" class="icons">
           <span>Calculatrice </span>
         </a>
         <a href="{{ url('rdv') }}" class="link">
-          <img width="30" height="31" src="{{ asset('svgs/notification.png') }}" class="icons">
+          <img src="{{ asset('svgs/bell.svg') }}" class="icons">
           <span>Notification</span>
         </a>
         @endrole
         @else
         @can('chat')
         <a href="{{ url('chat') }}" class="link">
-          <img src="{{ asset('svgs/chat-left-dots-fill.svg') }}" class="icons">
+          <img src="{{ asset('svgs/chat-left-dots.svg') }}" class="icons">
           <span>Chat</span>
         </a>
         <a href="{{ url('groupes') }}" class="link">
-          <img src="{{ asset('svgs/people-fill.svg') }}" class="icons">
+          <img src="{{ asset('svgs/people.svg') }}" class="icons">
           <span>Groupes</span>
         </a>
         @endcan
 
         @can('G-utilisateurs')
         <a href="{{ url('users') }}" class="link">
-          <img width="20" height="38" src="{{ asset('svgs/management.png') }}"
+          <img src="{{ asset('svgs/management.png') }}"
             class="icons">
           <span>Utilisateurs</span>
         </a>
@@ -140,18 +140,18 @@
 
         @can('G-conversations')
         <a href="{{ url('groupes') }}" class="link">
-          <img src="{{ asset('svgs/people-fill.svg') }}" class="icons">
+          <img src="{{ asset('svgs/people.svg') }}" class="icons">
           <span>Conversations</span>
         </a>
         @endcan
 
         @can('G-formulaires')
         <a href="/forms/create" class="link">
-          <img height="30" width="31" src="{{ asset('svgs/formulaire.png') }}" class="icons">
+          <img src="{{ asset('svgs/formulaire.png') }}" class="icons">
           <span>Créer formulaire</span>
         </a>
         <a href="/forms/sub" class="link">
-          <img height="30" width="31" src="{{ asset('svgs/remplis.png') }}" class="icons">
+          <img src="{{ asset('svgs/remplis.png') }}" class="icons">
           <span>Formulaires remplis</span>
         </a>
         @endcan
@@ -165,42 +165,42 @@
 
         @can('G-présence')
         <a href="/presence" class="link">
-          <img width="3" height="35" src="{{ asset('svgs/presence.png') }}" class="icons">
+          <img src="{{ asset('svgs/person-check.svg') }}" class="icons">
           <span>Tableau de présence</span>
         </a>
         @endcan
 
         @can('G-entretiens')
         <a href="/entretiens" class="link">
-          <img height="30" width="35" src="{{ asset('svgs/entretien.png') }}" class="icons">
+          <img src="{{ asset('svgs/person-workspace.svg') }}" class="icons">
           <span>Gestion des entretiens</span>
         </a>
         @endcan
 
         @can('G-formations')
         <a href="/formations" class="link">
-          <img width="36" height="38" src="{{ asset('svgs/formation.png') }}" class="icons">
+          <img src="{{ asset('svgs/mortarboard.svg') }}" class="icons">
           <span>Gestion de formations</span>
         </a>
         @endcan
 
         @can('Calcule-salaire')
         <a href="{{ url('calculatrice') }}" class="link">
-          <img width="38" height="35" src="{{ asset('svgs/calculatrice.png') }}" class="icons">
+          <img src="{{ asset('svgs/calculator.svg') }}" class="icons">
           <span>Calculatrice </span>
         </a>
         @endcan
 
         @can('Calcule-assiduité ')
         <a href="{{ url('calculatrice') }}" class="link">
-          <img width="38" height="35" src="{{ asset('svgs/calculatrice.png') }}" class="icons">
+          <img src="{{ asset('svgs/calculator.svg') }}" class="icons">
           <span>Calculatrice </span>
         </a>
         @endcan
 
         @can('Calcule-prime')
         <a href="{{ url('calculatrice') }}" class="link">
-          <img width="38" height="35" src="{{ asset('svgs/calculatrice.png') }}" class="icons">
+          <img src="{{ asset('svgs/calculator.svg') }}" class="icons">
           <span>Calculatrice </span>
         </a>
         @endcan
@@ -214,7 +214,7 @@
 
         @can('G-codes')
         <a href="{{ url('codes') }}" class="link">
-          <img src="{{ asset('svgs/people-fill.svg') }}" class="icons">
+          <img src="{{ asset('svgs/people.svg') }}" class="icons">
           <span>Codes</span>
         </a>
         @endcan
@@ -235,14 +235,13 @@
 
         @can('notification')
         <a href="{{ url('rdv') }}" class="link">
-          <img width="30" height="31" src="{{ asset('svgs/notification.png') }}" class="icons">
+          <img src="{{ asset('svgs/bell.svg') }}" class="icons">
           <span>Notification</span>
         </a>
         @endcan
         @endif
         <a onclick="logout()" class="link">
-          <img width="30" height="38" src="{{ asset('svgs/logout.png') }}"
-            class="icons">
+          <img src="{{ asset('svgs/box-arrow-left.svg') }}" class="icons">
           <form method="POST" id="logout-form" action="/logout">@csrf</form>
           <span>Logout</span>
         </a>
