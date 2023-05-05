f84ebdefc28d21553689f05776c4c3f0d55a0fbd
Sifeddine reguragui <sifeddine.regragui@gmail.com>
2023-05-05 10:59:22 +0000

    email verf removed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -152,23 +152,6 @@ class UserController extends Controller
         ));
         $response = curl_exec($curl);
         curl_close($curl);
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://api.apilayer.com/email_verification/check?email=$request->email",
-            CURLOPT_HTTPHEADER => array(
-                "Content-Type: text/plain",
-                "apikey: rFRHFae64HfjewxhSicqSzN2LQih0zpV"
-            ),
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET"
-        ));
-        $response = curl_exec($curl);
-        curl_close($curl);
         
         if(json_decode($response)->smtp_check){
             $user->name = $request->name ;
--- resources/views/forms/liste.blade.php
+++ resources/views/forms/liste.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.sidebar')
 @section('content')
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <link rel="stylesheet" href="{{ asset('css/liste_form.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/liste_form.css') }}"> 
   <div class="form_list">
     <h1>Liste Des formulaires</h1>
     <div class="forms">
       @forelse ($forms as $form)
         <a href="/forms/{{$form->id}}" class="form">
+          <img src="../svgs/trash.svg" class="del_qst icons" alt="">
           <h4>{{$form->name}}</h4>
           <small>{{$form->created_at}}</small>
         </a>
